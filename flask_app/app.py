b57fb4c3dbdf1147c2531fc9519e40e94f3fc6ac
realhardik18 <realhardik18@gmail.com>
2022-08-16 02:14:26 +0530

    now app can login with twitter
    working on other parts now

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,25 @@
-from flask import render_template, Flask
+import requests as r
+import json
+import ast
+from flask import render_template, Flask, redirect, url_for, request
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+from creds import API_KEY, API_SECRET, APP_SECRET_KEY
 
-app = Flask('')
+app = Flask(__name__)
+app.secret_key = APP_SECRET_KEY
+blueprint = make_twitter_blueprint(
+    api_key=API_KEY,
+    api_secret=API_SECRET,
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    resp = twitter.get("https://api.twitter.com/2/users/me")
+    return str(resp.text)
 
 
-app.run()
+app.run(debug=True)
