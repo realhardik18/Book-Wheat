28af9c48c3692e50d32cb1e3b1ddea3e91f5aeaf
realhardik18 <realhardik18@gmail.com>
2022-08-15 14:21:24 +0530

    added some db methods

--- db.py
+++
@@ -1,7 +0,0 @@
-from ast import Delete
-import redis
-from creds import REDIS_PASSWORD, REDIS_HOST, REDIS_PORT
-
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD,)
-
-# add stuff with r.set and work on retriving for the same
---
+++ db_methods.py
@@ -0,0 +1,65 @@
+import ast
+import redis
+from creds import REDIS_HOST, REDIS_PASSWORD, REDIS_PORT
+
+redis_client = redis.Redis(
+    host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
+
+def DeleteAllUsers():
+    for key in redis_client.scan_iter():
+        redis_client.delete(key)
+
+
+def ShowAllUsers():
+    users = list()
+    for user in redis_client.keys():
+        users.append(str(user)[2:-1])
+    return users
+
+
+def AddUser(user_id, username):
+    initial_user_data = {
+        'username': username,
+        'categories': list()
+    }
+    redis_client.set(name=str(user_id), value=str(initial_user_data))
+
+
+def ShowSpecifcUserData(user_id):
+    return ast.literal_eval(str(redis_client.get(name=str(user_id)))[2:-1])
+
+
+def GetAllCategories(user_id):
+    data = ShowSpecifcUserData(user_id)
+    return data['categories']
+
+
+def AddCategory(user_id, category_name):
+    data = ShowSpecifcUserData(user_id)
+    category_exists = False
+    for category in data['categories']:
+        if category['name'] == category_name:
+            category_exists = True
+    if len(data['categories']) == 0 or category_exists == False:
+        new_category_data = {
+            'name': category_name,
+            'data': list()
+        }
+        data['categories'].append(new_category_data)
+        redis_client.set(user_id, str(data))
+    elif category_exists == True:
+        return 'exists'
+
+
+def DeleteUser(user_id):
+    redis_client.delete(str(user_id))
+
+
+def AddTweetInCategory(user_id, category_name, url_to_tweet):
+    data = ShowSpecifcUserData(user_id)
+    for category in data['categories']:
+        if category['name'] == category_name:
+            category['data'].append(url_to_tweet)
+            redis_client.set(user_id, str(data))
+            break
